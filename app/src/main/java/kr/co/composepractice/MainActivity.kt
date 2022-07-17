899c084340d72a2a15ed8a585c25e530da74c26c
hyosikchoi <huchoi24@naver.com>
2022-07-17 23:29:02 +0900

    Constraint Layout practice

--- app/src/main/java/kr/co/composepractice/MainActivity.kt
+++ app/src/main/java/kr/co/composepractice/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import kr.co.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Android","Compose")
                 }
             }
         }
@@ -33,13 +35,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(100.dp)
+fun Greeting(name: String, compose : String) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
     ) {
-        Text(text = name)
-        Text(modifier = Modifier.align(Alignment.BottomEnd), text = name)
+        val (textOne, textTwo) = createRefs()
+
+        Text(
+            text = name,
+            modifier = Modifier.constrainAs(textOne) {
+                top.linkTo(
+                    parent.top
+                )
+                bottom.linkTo(
+                    parent.bottom
+                )
+            } // top , bottom 을 parent에 맞춰서 vertical을 중간으로 맞춤.
+        )
+
+        Text(
+            text = compose,
+            modifier = Modifier.constrainAs(textTwo) {
+                top.linkTo(
+                    textOne.bottom,
+                    margin = 10.dp
+                ) // 해당 컨텐츠의 top 을 textOne의 bottom으로 그리고 margin 10dp 준다.
+            }
+        )
+
     }
 }
 
@@ -47,6 +72,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        Greeting("Android")
+        Greeting("Android","Compose")
     }
 }
