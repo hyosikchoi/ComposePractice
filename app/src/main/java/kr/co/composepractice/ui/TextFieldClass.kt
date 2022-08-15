891b548845b301c9188c723b804312688c9c8fd6
hyosikchoi <huchoi24@naver.com>
2022-08-15 21:17:57 +0900

    TextField practice with BoxWithConstraints Layout

---
+++ app/src/main/java/kr/co/composepractice/ui/TextFieldClass.kt
@@ -0,0 +1,39 @@
+package kr.co.composepractice.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class TextFieldClass : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            textField()
+        }
+
+    }
+
+    @Composable
+    fun textField() {
+        val textState = remember {
+            mutableStateOf("")
+        }
+
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            TextField(modifier = Modifier.align(Alignment.Center), value = textState.value, onValueChange = { textValue -> textState.value = textValue} )
+        }
+
+    }
+
+}
